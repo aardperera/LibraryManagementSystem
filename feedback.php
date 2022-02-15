a86bc37cc47e95b36607acc92b9f27dea89527f5
aardperera <randiniama1@gmail.com>
2022-02-15 12:54:25 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -22,13 +22,50 @@
                 background-repeat: no-repeat;
                
             }
+           
 
+            #box4{
+                height: 540px;
+                width: 800px;
+                background-color: rgb(0, 0, 0);
+                margin: 0px auto;
+                opacity: 0.7;
+                color: white;
+                padding-right: 40px;
+                padding: 20px;
+            }
+           
         </style>
         
     </head>
     <body>
        
         <section>
+            
+                <div id="box4">
+                    <h2 style="text-align: center">How was your experience?</h2>
+                    Please take a moment to leave a feedback!<br>
+                    If you have any suggessions or questions, Comment below.<br><br>
+
+                    <form name="feedback" action="" method="post">
+                        <input class="form-control" type="text" name="comments" placeholder="Write something..."><br>
+                        <input class="btn btn-primary" type="submit" name="submit" value="Comment"><br>
+                    </form>
+                </div>
+
+                <div>
+                    <?php
+                        if(isset($_POST['submit'])){
+                            $sql="INSERT INTO `comment` VALUES('$_POST[comments]');";
+                            if(mysqli_query($db,$sql)){
+                                $sq="SELECT * FROM `comment` ORDER BY `comment`.`id` DESC;";
+                            }
+                        }
+                        
+                    
+                    ?>
+                </div>
+            
            
         </section> 
     
@@ -37,3 +74,5 @@
         </footer>
     </body>
 </html>
+
+
