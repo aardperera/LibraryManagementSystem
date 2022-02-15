b8248f228167aa6f9163617bb1aba91e5b7a4571
aardperera <randiniama1@gmail.com>
2022-02-15 12:45:40 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,3 @@
 <?php
     $db = mysqli_connect("localhost","root","","library");
-
-    if(!$db){
-        die("Connection failed:".mysqli_connect_error());
-    }else{
-        echo "Connected successfully.";
-    }
-
 ?>
