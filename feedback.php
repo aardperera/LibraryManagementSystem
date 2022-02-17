4628e938e994323d2b017d616c671212794615b1
aardperera <randiniama1@gmail.com>
2022-02-17 16:43:35 +0530

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -22,11 +22,10 @@
                 background-repeat: no-repeat;
                
             }
-           
 
-            #box4{
+            .box4{
                 height: 540px;
-                width: 800px;
+                width: 700px;
                 background-color: rgb(0, 0, 0);
                 margin: 0px auto;
                 opacity: 0.7;
@@ -34,7 +33,11 @@
                 padding-right: 40px;
                 padding: 20px;
             }
-           
+            .scroll{
+                height: 200px;
+                width: 100%;
+                overflow: auto;
+            }
         </style>
         
     </head>
@@ -42,30 +45,46 @@
        
         <section>
             
-                <div id="box4">
-                    <h2 style="text-align: center">How was your experience?</h2>
-                    Please take a moment to leave a feedback!<br>
-                    If you have any suggessions or questions, Comment below.<br><br>
+            <div class="box4">
+                <h2 style="text-align: center">How was your experience?</h2>
+                    Please take a moment to leave a feedback!
+                    If you have any suggessions or questions,<br> Comment below.<br><br>
 
-                    <form name="feedback" action="" method="post">
-                        <input class="form-control" type="text" name="comments" placeholder="Write something..."><br>
-                        <input class="btn btn-primary" type="submit" name="submit" value="Comment"><br>
-                    </form>
-                </div>
-
-                <div>
+                <form name="feedback" action="" method="post">
+                    <input class="form-control" type="text" name="comments" placeholder="Write something..."><br>
+                    <input class="btn btn-primary" type="submit" name="submit" value="Comment"><br>
+                </form>
+               <br>
+                <div class="scroll">
                     <?php
                         if(isset($_POST['submit'])){
-                            $sql="INSERT INTO `comment` VALUES('$_POST[comments]');";
+                            $sql = "INSERT INTO `comment`(`cmid`, `comments`) VALUES (null,'$_POST[comments]');";
                             if(mysqli_query($db,$sql)){
-                                $sq="SELECT * FROM `comment` ORDER BY `comment`.`id` DESC;";
+                                $q= "SELECT * FROM `comment` ORDER BY `comment`.`cmid` DESC;";
+                                $res = mysqli_query($db,$q);
+
+                                echo "<table class = 'table table-bordered'>";
+                                while($row=mysqli_fetch_assoc($res)){
+                                    echo "<tr>";
+                                        echo "<td>"; echo $row['comments']; echo "</td>";
+                                    echo "</tr>";
+                                }
                             }
                         }
-                        
-                    
+                        else{
+                            $q= "SELECT * FROM `comment` ORDER BY `comment`.`cmid` DESC;";
+                            $res = mysqli_query($db,$q);
+
+                            echo "<table class = 'table table-bordered'>";
+                            while($row=mysqli_fetch_assoc($res)){
+                                echo "<tr>";
+                                    echo "<td>"; echo $row['comments']; echo "</td>";
+                                echo "</tr>";
+                            }
+                        }
                     ?>
                 </div>
-            
+            </div>    
            
         </section> 
     
