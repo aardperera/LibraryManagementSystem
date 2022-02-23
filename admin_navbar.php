3bbbfb8e48edb098470795a2114c3235e7761bf8
aardperera <randiniama1@gmail.com>
2022-02-24 00:20:21 +0530

    Update admin_navbar.php

--- admin_navbar.php
+++ admin_navbar.php
@@ -36,8 +36,8 @@
                         <li><a href="">
                             <div style="color: white;">
                                 <?php
-                                    echo "<img class = 'img-circle profile_img' src= 'images/".$_SESSION['pic']."'>";
-                                    echo $_SESSION['login_user'];
+                                    echo "<img class = 'img-circle profile_img' height = 30 width=30 src= 'images/".$_SESSION['pic']."'>";
+                                    echo " ".$_SESSION['login_user'];
                                 ?>
                             </div>
                         </a></li>
