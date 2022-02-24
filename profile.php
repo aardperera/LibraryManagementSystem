159c2221568c1519c1ba2d9c1ce6cff3098a7661
aardperera <randiniama1@gmail.com>
2022-02-24 23:35:17 +0530

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+    include "connection.php";
+    include "navbar.php";
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body style="background-color: #004528;">
+        <div class="container">
+            <form action="" method="post">
+                <button class="btn btn-default" style="float:right; width:70px;" name="edit">Edit</button>
+            </form>
+        </div>
+    </body>
+</html>
